8f95610282054b5c26705d7528ed701ea286a9fb
Dmitry <dimakondykov@gmail.com>
2026-07-29 21:45:16 +0300

    fix deploy

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
-ALLOWED_HOSTS = ["158.160.204.163","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["158.160.204.163", "localhost", "127.0.0.1"]
 
 
 INSTALLED_APPS = [
