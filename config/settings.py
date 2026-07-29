be0a2405007251a16cebfdb1a443fb0d7d36d959
Dmitry <dimakondykov@gmail.com>
2026-07-29 21:40:05 +0300

    fix.update settings and docker-compose and secrets

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,8 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["158.160.204.163","localhost","127.0.0.1"]
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
